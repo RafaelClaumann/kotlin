b2c6ff69129f12698983077c4ea587ae80378b17
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2024-09-23 08:44:43 -0300

    kb: variaveis mutaveis e imutaveis

--- kotlin_basico/src/main/kotlin/Main.kt
+++ kotlin_basico/src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 package com.claumann
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    // mutavel
+    var nome = "rafael"
+    nome = "claumann"
+
+    // imutavel
+    val  idade = 28
+    // idade = 30 // 'val' cannot be reassigned.
+
+    println(nome)
+    print(idade)
 }
