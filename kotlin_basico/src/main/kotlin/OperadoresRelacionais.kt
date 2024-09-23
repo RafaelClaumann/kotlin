9d6f53a2d11971e68320ccaa373b56fb6a1a8bb4
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2024-09-23 14:36:23 -0300

    kb: operadores relacionais == e ===

--- kotlin_basico/src/main/kotlin/OperadoresRelacionais.kt
+++ kotlin_basico/src/main/kotlin/OperadoresRelacionais.kt
@@ -29,4 +29,43 @@ fun main() {
         println("foo($foo) é maior ou igual a fee($fee)")
     }
 
+    /**
+     * Diferença entre == e ===
+     *  == : Verifica se os valores dos objetos são equivalentes.
+     *       É útil quando você deseja comparar o conteúdo dos objetos.
+     *  ===: Verifica se os dois operandos referenciam o mesmo objeto.
+     *       É útil quando você precisa saber se duas variáveis estão apontando para o mesmo local na memória.
+     *
+     *  Para tipos primitivos (como Int, Boolean, etc.), === e == têm o mesmo efeito, pois esses tipos são
+     *  representados como valores e não como referências.
+     */
+    val a = Carro("Fusca", 1973)
+    val b = Carro("Fusca", 1973)
+
+    // TRUE
+    // São referencias diferentes, mas seus atributos são iguais
+    // e, portanto, o metodo equals de Carro retorna true
+    if (a == b) {
+        println("$a é igual a $b usando ==")
+    }
+
+    // FALSE
+    // Apesar de possuirem atributos com mesmos valores, a e b são
+    // referencias diferentes
+    if (a === b) {
+        println("$a é igual a $b usando ===")
+    }
+
+}
+
+class Carro(val modelo: String, val ano: Int) {
+    fun mostrarDetalhes() {
+        println("Modelo: $modelo, Ano: $ano")
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true // Verifica se as referências são iguais
+        if (other !is Carro) return false // Verifica se o objeto é da mesma classe
+        return modelo == other.modelo && ano == other.ano // Compara as propriedades
+    }
 }
